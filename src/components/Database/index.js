053561c5acb41afaa0ec02c28514479789ac8a11
FilipRazek <filip.razek@protonmail.com>
2020-05-23 23:23:25 +0200

    Change name and add database activity

---
+++ src/components/Database/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Header from '../Header'
+import './index.css'
+
+export default () => {
+  return (
+    <div className='database__body'>
+      <Header title='Database' />
+      <p>Database</p>
+    </div>
+  )
+}
--- src/components/HomeRouter/index.js
+++ src/components/HomeRouter/index.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import AboutPage from '../AboutPage'
+import Database from '../Database'
 import HelpPage from '../HelpPage'
 import Home from '../Home'
 import NumeralConverter from '../NumeralConverter'
@@ -10,6 +11,7 @@ import TwentyThreeOrBust from '../TwentyThreeOrBust'
 
 const HomeRouter = () => {
   const links = [
+    { path: '/database', label: 'Database', component: <Database /> },
     { path: '/morse', label: 'Morse', component: <Morse /> },
     {
       path: '/twenty-three-or-bust',
